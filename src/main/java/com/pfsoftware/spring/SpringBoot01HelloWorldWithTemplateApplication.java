5c6616a91752e3efd40df742ce74686f89df69b1
Dave Wolf <dave45678@gmail.com>
2017-08-08 15:33:32 -0400

    initial upload

---
+++ src/main/java/com/pfsoftware/spring/SpringBoot01HelloWorldWithTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.pfsoftware.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot01HelloWorldWithTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot01HelloWorldWithTemplateApplication.class, args);
+	}
+}
